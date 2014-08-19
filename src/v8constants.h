9735bf9946fa82433c7e25516f2d58d1d1472d71
Fedor Indutny <fedor@indutny.com>
2014-08-19 12:11:27 +0400

    v8constants: another polyfill on v0.10

--- src/v8constants.h
+++ src/v8constants.h
@@ -50,7 +50,6 @@ static const int kCDV8MapFieldCount = 2;
     X(class_JSFunction__shared__SharedFunctionInfo)                           \
     X(class_JSMap__table__Object)                                             \
     X(class_JSObject__properties__FixedArray)                                 \
-    X(class_Map__bit_field2__char)                                            \
     X(class_Map__bit_field3__SMI)                                             \
     X(class_Map__code_cache__Object)                                          \
     X(class_Map__constructor__Object)                                         \
@@ -179,6 +178,7 @@ static const int kCDV8MapFieldCount = 2;
     X(type_JSWeakSet__JS_WEAK_SET_TYPE, -1)                                   \
     X(type_PropertyCell__PROPERTY_CELL_TYPE, -1)                              \
     X(OddballTheHole, 2)                                                      \
+    X(class_Map__bit_field2__char, class_Map__instance_attributes__int + 3)   \
 
 #define CD_V8_CONSTANT_VALUE(V) int cd_v8_##V;
 CD_V8_REQUIRED_CONSTANTS_ENUM(CD_V8_CONSTANT_VALUE);
